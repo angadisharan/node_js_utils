2bda732a102934a59d366a70db9a3486b8b9630a
Sharan <sharan@onlinerti.com>
2019-01-23 18:33:29 +0530

    0.0.2

--- index.js
+++ index.js
@@ -26,3 +26,42 @@ var isEmptyArray = function(value) {
 }
 module.exports.isEmptyArray  = isEmptyArray
 
+
+
+var extractNumberListFromString = function(value) {
+	if (isEmptyString(value)) {
+		return ""
+	}
+
+	return value.match(/[-+]?[0-9]*\.?[0-9]+/g)
+}
+module.exports.extractNumberListFromString = extractNumberListFromString
+
+
+var extractNumberFromString = function(value) {
+	if (isEmptyString(value)) {
+		return ""
+	}
+
+	var numberList = value.match(/[-+]?[0-9]*\.?[0-9]+/g);
+	if (isEmptyArray(numberList)) {
+		return ""
+	}
+	return numberList[0]
+}
+module.exports.extractNumberFromString  = extractNumberFromString
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -3,4 +3,7 @@ var s_validate = require('./index')
 var test = "   "
 // console.log(s_validate.isEmptyString(" /"))
 // console.log(s_validate.isEmptyString("-1"))
-console.log(s_validate.isEmptyArray([{}, {}]))
+// console.log(s_validate.isEmptyArray([{}, {}]))
+
+
+console.log(s_validate.extractNumberFromString(""))
